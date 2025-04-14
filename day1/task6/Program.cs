d6b1ae57e5e6e751e1b7646ab60f7406b28b0afc
murrkmeow <kostyarimsha3@gmail.com>
2025-04-14 14:26:40 +0300

    done

---
+++ day1/task6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите трехзначное число: ");
+            int number = int.Parse(Console.ReadLine()!);
+
+            if (number < 100 || number > 999)
+            {
+                Console.WriteLine("Ошибка: число должно быть трехзначным.");
+            }
+            else
+            {
+                int digit1 = number % 10;           
+                int digit2 = (number / 10) % 10;    
+                int digit3 = number / 100;          
+
+                int reversed = digit1 * 100 + digit2 * 10 + digit3;
+
+                Console.WriteLine("Число, прочитанное справа налево: " + reversed);
+            }
+        }
+    }
+}
