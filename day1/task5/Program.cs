3c39d6e2a05f9abe9e3ca929b6676d530867960d
murrkmeow <kostyarimsha3@gmail.com>
2025-04-14 14:24:32 +0300

    done

---
+++ day1/task5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Цена одного килограмма яблок (руб.): ");
+            double pricePerKg = double.Parse(Console.ReadLine()!.Replace('.', ','));
+
+            Console.Write("Вес яблок (кг): ");
+            double weight = double.Parse(Console.ReadLine()!.Replace('.', ','));
+
+            double totalCost = pricePerKg * weight;
+
+            Console.WriteLine("Стоимость покупки: {0:0.00} руб.", totalCost);
+        }
+    }
+}
