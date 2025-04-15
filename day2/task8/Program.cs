7699d1a9adb5e9fa822a23cc7bba3acd132dd7a8
murrkmeow <kostyarimsha3@gmail.com>
2025-04-15 19:30:57 +0300

    done

---
+++ day2/task8/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа k и n подряд (1 <= k, n <= 100):");
+            int k = int.Parse(Console.ReadLine()!);
+            int n = int.Parse(Console.ReadLine()!);
+
+            if (1 <= k && n <= 100)
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
