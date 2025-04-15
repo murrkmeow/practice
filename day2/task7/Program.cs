f5fbb91a0d123c1fffbe3665cb20891e722c93d8
murrkmeow <kostyarimsha3@gmail.com>
2025-04-15 19:20:06 +0300

    done

---
+++ day2/task7/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Таблицы перевода дюймов в сантиметры:");
+
+            double d = 25.4f;
+
+            for (int i = 2; i <= 24; i += 2)
+            {
+                Console.WriteLine($"{i} д. = {d * i} сант.");
+            }
+
+            Console.WriteLine("_____________________________\n");
+
+            int j = 2;
+
+            while (j <= 24)
+            {
+                Console.WriteLine($"{j} д. = {d * j} сант.");
+                j += 2;
+            }
+
+            Console.WriteLine("_____________________________\n");
+            
+            int t = 2;
+
+            do
+            {
+                Console.WriteLine($"{t} д. = {d * t} сант.");
+                t += 2;
+            } while (t <= 24);
+        }
+    }
+}
