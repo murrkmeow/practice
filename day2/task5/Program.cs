ab2fe33e75bdeb028a2b4babdb2c7a5c59120cb3
murrkmeow <kostyarimsha3@gmail.com>
2025-04-15 18:33:23 +0300

    done

---
+++ day2/task5/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите четырехзначное число: ");
+            int num = int.Parse(Console.ReadLine()!);
+
+            if (num >= 1000 && num < 10000)
+            {
+                int first = num / 1000;
+                int second = num % 1000 / 100;
+
+                if (first > second)
+                {
+                    Console.WriteLine($"Первая цифра больше. ({first} > {second})");
+                }
+                else if (first < second)
+                {
+                    Console.WriteLine($"Вторая цифра больше. ({first} < {second})");
+                }
+                else
+                {
+                    Console.WriteLine($"Цифры равны. ({first} = {second})");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Число не четырехзначное.");
+            }
+        }
+    }
+}
