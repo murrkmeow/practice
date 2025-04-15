38b3d41e91011bda2a4bcaceda221b59e5e7dd5d
murrkmeow <kostyarimsha3@gmail.com>
2025-04-15 18:19:29 +0300

    done

---
+++ day2/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значния ОТ и ДО подряд: ");
+            int a = int.Parse(Console.ReadLine()!);
+            int b = int.Parse(Console.ReadLine()!);
+            
+            if (a < b)
+            {
+                int sum = 0;
+                for (int i = a; i <= b; i++)
+                {
+                    sum += i;
+                }
+                Console.WriteLine($"Сумма чисел от {a} до {b}: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Число А > В");
+            }
+        }
+    }
+}
