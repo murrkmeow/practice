6d6dcbf369a9bddfb0ded078842115193c6643d2
murrkmeow <kostyarimsha3@gmail.com>
2025-04-15 18:22:58 +0300

    done

---
+++ day2/task4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace task4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите значение x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y;
+
+            if (x < 0.1)
+            {
+                y = Math.Sqrt(2 * Math.Pow(x, 2) + Math.Sin(x) + 1);
+                Console.WriteLine($"Результат функции: y = {y}");
+            }
+            else if (x == 0.1)
+            {
+                y = 2 * x + Math.Exp(x);
+                Console.WriteLine($"Результат функции: y = {y}");
+            }
+            else
+            {
+                Console.WriteLine("Функция не определена для значений x > 0.1.");
+            }
+        }
+    }
+}
