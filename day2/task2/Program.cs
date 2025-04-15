f06970a41c73c8e6363557960e954668482745cd
murrkmeow <kostyarimsha3@gmail.com>
2025-04-15 17:34:56 +0300

    done

---
+++ day2/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите три числа подряд:");
+            int a = int.Parse(Console.ReadLine()!);
+            int b = int.Parse(Console.ReadLine()!);
+            int c = int.Parse(Console.ReadLine()!);
+
+            if (a == b && a == c)
+                Console.WriteLine("Все числа равны.");
+            else if (a == b || a == c || a == b)
+                Console.WriteLine("Одна пара чисел равна.");
+            else
+                Console.WriteLine("Ни одно число не равно другому.");
+        }
+    }
+}
