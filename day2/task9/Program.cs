94af8f9679e1eac3ad0ca37effc13c31f1081a58
murrkmeow <kostyarimsha3@gmail.com>
2025-04-15 19:38:57 +0300

    done

---
+++ day2/task9/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 0, B = 1;
+            int M = 20;
+            double H = (B - A) / M;
+
+            Console.WriteLine("Таблица значений функции arctan(x):");
+            for (int i = 0; i <= M; i++)
+            {
+                double x = A + i * H;
+                double y = Math.Atan(x);
+                Console.WriteLine($"x = {x:F4}, arctan(x) = {y:F4}");
+            }
+        }
+    }
+}
