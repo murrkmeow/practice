0e07ace2e3aec516c7f0daf36a02eb4ec4093d64
murrkmeow <kostyarimsha3@gmail.com>
2025-04-17 08:50:40 +0300

    done

---
+++ day4/task1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static double CalculateFx(double x, double a, double b)
+        {
+            if (Math.Abs(x) < 1)
+            {
+                return Math.Tan(b - x);
+            }
+            else if (Math.Abs(x) >= 1 && Math.Abs(x) <= 5)
+            {
+                return a / Math.Sqrt(x * x);
+            }
+            else // Math.Abs(x) > 5
+            {
+                return 1 / Math.Sqrt(x * x + a + Math.Cos(b * x));
+            }
+        }
+
+        static void Main()
+        {
+            Console.Write("Введите значение a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите шаг h: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nТаблица значений функции:");
+            Console.WriteLine("{0,-10}{1,-10}", "x", "f(x)");
+            Console.WriteLine(new string('-', 20));
+
+            for (double x = a; x <= b; x += h)
+            {
+                try
+                {
+                    double y = CalculateFx(x, a, b);
+                    Console.WriteLine("{0,-10:F2}{1,-10:F2}", x, y);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("{0,-10:F2}{1,-10}", x, "деление на 0!");
+                }
+            }
+        }
+    }
+}
